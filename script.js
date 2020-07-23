43367470ff194090edb602ced31bdd6865d17022
Adam <galekxy13@hotmail.com>
2020-07-23 19:59:49 -0400

    added flipping to the cards, with logos on front and roster on the back

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ let container = document.querySelector('.container');
 
 
 
-function createCard(teamName, playerName, playerNumber, playerPosition) {
 
+function createCard(teamName, teamId, playerId, playerNumber, playerPosition) {
+
+    console.log(teamId, 'team id')
 
     // Create the div that will be the structure of the card
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
+    
+    const innerCard = document.createElement('div');
+    innerCard.classList.add('card-inner');
+
+    const frontCard = document.createElement('div');
+    frontCard.classList.add('card-front');
+    const image = document.createElement('img');
+    image.src = `./Assets/${teamId}.png`;
+    frontCard.appendChild(image);
+    
+    const backCard = document.createElement('div');
+    backCard.classList.add('card-back');
 
     // Create the H3 of the card
     const h3 = document.createElement('h3');
     h3.innerText = `${teamName}`;
-    cardDiv.appendChild(h3)
+    backCard.appendChild(h3)
     // Create the Table of the card
     const table = document.createElement('table');
+    // Creating the header of the Table
     const thead = document.createElement('thead');
     let th = document.createElement('th');
     let thText = document.createTextNode('Name');
@@ -33,11 +48,19 @@ function createCard(teamName, playerName, playerNumber, playerPosition) {
     thead.appendChild(row);
     table.appendChild(thead);
 
+    // Adding the content of the table
     const tbody = document.createElement('tbody');
     tbody.id = `${teamName}`;
+
     table.appendChild(tbody);
 
-    cardDiv.appendChild(table);
+    // appending the Table onto the card
+    backCard.appendChild(table);
+    innerCard.appendChild(backCard);
+    innerCard.appendChild(frontCard);
+    cardDiv.appendChild(innerCard);
+
+    // adding the card onto the container
     container.appendChild(cardDiv);
 }
 
@@ -64,10 +87,10 @@ async function getTeamRosterData(id) {
     let data = await response.json();
     let rosterData = data.teams[0].roster.roster;
     let teamName = data.teams[0].name;
-    console.log(rosterData);
+    console.log(rosterData, 'roster data');
 
 
-    createCard(teamName);
+    createCard(teamName, id);
 
     // document.getElementById('teamName').innerText = teamName
 
@@ -75,11 +98,12 @@ async function getTeamRosterData(id) {
         let card = '';
 
         rosterData.forEach(player => {
+            // getPlayerData(player.person.id);
             card +="<tr>";
-            card += `<td>${player.person.fullName}</td>`;
+            card += `<td><a href="https://statsapi.web.nhl.com/api/v1/people/${player.person.id}/stats?stats=gameLog&season=20192020">${player.person.fullName}</a></td>`;
+            // card += `<td><button type="button" class="collapsible">${player.person.fullName}</button></td>`;
             card += `<td>${player.jerseyNumber}</td>`;
             card += `<td>${player.position.name}</td></tr>`
-           
         });
         document.getElementById(`${teamName}`).innerHTML = card
     }
@@ -89,8 +113,8 @@ async function getTeamRosterData(id) {
 // getTeamRosterData()
 
 
-async function getPlayerData() {
-    let response = await fetch('https://statsapi.web.nhl.com/api/v1/people/8471698/stats?stats=gameLog&season=20182019');
+async function getPlayerData(id) {
+    let response = await fetch(`https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=gameLog&season=20182019`);
     let data = await response.json();
     let playerData = data.stats[0].splits;
 
@@ -112,3 +136,4 @@ async function getPlayerData() {
 }
 
 // getPlayerData()
+
